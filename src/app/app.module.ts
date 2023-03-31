1a90a2b8267ab826dbaab7fee71e254b35f529ba
Mahmoud Farahat <mahmoud.ped@gmail.com>
2023-03-31 17:35:37 +0200

    1.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardFormComponent } from './card-form/card-form.component';
+import { InputComponent } from './input/input.component';
+import { NgxMaskModule, IConfig } from 'ngx-mask';
+import { CardComponent } from './card/card.component'
+
+export const options: Partial<null|IConfig> | (() => Partial<IConfig>) = null;
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardFormComponent,
+    InputComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NgxMaskModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/input/input.component.ts
@@ -0,0 +1,24 @@
+import { FormControl } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent implements OnInit {
+@Input() label!:string
+@Input() control: FormControl | any
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+showErrors(){
+  const {dirty , touched , errors} = this.control
+  return dirty  && touched && errors
+}
+
+}
